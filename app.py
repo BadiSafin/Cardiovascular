7dd5920c8c36a811eb727d01c4bad9a2a2a4be1a
Badi Safin <24010101608@darshan.ac.in>
2026-03-02 22:43:29 +0530

    first commit

--- app.py
+++ app.py
@@ -162,8 +162,13 @@ def predict():
         }), 400
 # =========================
 # RUN SERVER
-# =========================
+# # =========================
+# if __name__ == "__main__":
+#     app.run(host="localhost", port=5000, debug=True)
+
+
+import os
+
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
-
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
